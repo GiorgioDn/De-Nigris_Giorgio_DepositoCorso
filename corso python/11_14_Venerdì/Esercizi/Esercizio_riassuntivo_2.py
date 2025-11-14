86adbddf5eb5609c70de10068fcb17a5bf433792
GiorgioDn <128035358+GiorgioDn@users.noreply.github.com>
2025-11-14 16:22:40 +0100

    Esercizio con bozza decoratori

--- corso python/11_14_Venerdì/Esercizi/Esercizio_riassuntivo_2.py
+++ corso python/11_14_Venerdì/Esercizi/Esercizio_riassuntivo_2.py
@@ -1,1 +1,21 @@
 from Esercizio_riassuntivo_1 import *
+
+def d_controllo_negativi(funzione):
+    def wrapper(*args, **kwargs):
+        if args[0]<0:
+            args[0] = abs(args[0])
+        result = funzione(*args, **kwargs)
+        return result
+    return wrapper
+
+def d_controllo_lista(funzione):
+    def wrapper(*args, **kwargs):
+        if len(args[1]) !=0:
+            if funzione.__name__ == "":
+                result = funzione(*args, **kwargs)
+                return result
+        else:
+            if funzione.__name__ == "":
+                result = funzione(*args, **kwargs)
+                return result
+    return wrapper
