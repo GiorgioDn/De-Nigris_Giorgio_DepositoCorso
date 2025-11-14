18e54c5617d5f5024b4bc7b0ab3d4f7e39937fd6
GiorgioDn <128035358+GiorgioDn@users.noreply.github.com>
2025-11-14 17:58:41 +0100

    Change esercizi riassuntivi

--- corso python/11_14_Venerdì/Esercizi/Esercizio_riassuntivo_1.py
+++ corso python/11_14_Venerdì/Esercizi/Esercizio_riassuntivo_1.py
@@ -1,17 +1,58 @@
 """ 
     Creare una funzione che prende un intero positivo per generare una sottolista dei primi n elementi di una lista definita dall'utente, inoltre la funzione deve far scegliere se restituire la sottolista, eliminare i duplicati o verificare le eventuali differenze tra la lista e la sottolista.
 """
+from random import randint
+
 #funzione per eseguire le funzionalità dell'esercizio
 def subList (n, list):
     
-    #inizializzazione nuova lista
+    #controllo che n sia minore della lunghezza della lista
+    if len(list)>n:
+        #inizializzazione nuova lista
+        new_list = []
+        end = 0
+        
+        #riempio la nuova lista
+        while end < n:
+            new_list.append(list[end])
+            end +=1
+        
+        #scelta dell'operazione
+        chooice = int(input("Scegliere una delle seguenti operazioni: \n 1. Restituire la sottolista \n 2. Eliminare i duplicati della sottolista\n 3. Verificare gli elementi diversi della sottolista \n"))
+        
+        match chooice:
+            case 1:
+                return new_list
+            case 2:
+                #conversione per eliminare i duplicati
+                list_without_double = set(new_list)
+                return list_without_double
+            case 3:
+                #faccio la comparazione delle due liste
+                set_new = set(new_list)
+                set_ori = set(list)
+                return set_ori.difference(set_new)
+            case _:
+                return False
+    else:
+        return False
+        
+def subList_withoutList (n):
+    
+    #inizializzazione nuova lista con numeri casuali
+    
     new_list = []
     end = 0
+    list = []
+    
+    #creo la lista iniziale con valori random
+    for n in range(randint(0, n+10)):
+        list.append(randint(0, n*2))
     
     #riempio la nuova lista
-    while end < n:
-        new_list.append(list[end])
-        end +=1
+        while end < n:
+            new_list.append(list[end])
+            end +=1
     
     #scelta dell'operazione
     chooice = int(input("Scegliere una delle seguenti operazioni: \n 1. Restituire la sottolista \n 2. Eliminare i duplicati della sottolista\n 3. Verificare gli elementi diversi della sottolista \n"))
@@ -31,6 +72,6 @@ def subList (n, list):
         case _:
             return False
     
-x = subList(4, [9, 2, 2, 4, 5, 1, 3, 4])
+x = subList(20, [9, 2, 2, 4, 5, 1, 3, 4])
 
 print(x)
